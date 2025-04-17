243446656a9dae5a8f19d2a3f65bcec7317e45de
abhisheknagpure <110444541+abhisheknagpure@users.noreply.github.com>
2025-04-18 01:56:29 +0530

    create sandbox-aft account with correct email

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "abhishek.nagpure@sentinelone.com"
+    AccountEmail              = "abhishek.nagpure.2@sentinelone.com"
     AccountName               = "sandbox-aft"
     ManagedOrganizationalUnit = "Sandbox"
     SSOUserEmail              = "abhishek.nagpure@sentinelone.com"
