9892e744ce6a478254463e8c557f8394e4ec176c
abhisheknagpure <110444541+abhisheknagpure@users.noreply.github.com>
2025-04-21 15:49:16 +0530

    Add Audit account

---
+++ terraform/audit.tf
@@ -0,0 +1,27 @@
+module "audit" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "piotr.szczepanski+audit@sentinelone.com"
+    AccountName               = "Audit"
+    ManagedOrganizationalUnit = "Security"
+    SSOUserEmail              = "abhishek.nagpure@sentinelone.com"
+    SSOUserFirstName          = "abhishek"
+    SSOUserLastName           = "nagpure"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "abhishek nagpure"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
